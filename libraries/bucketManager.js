5612441ebf544f1bfe2c6f850e6406974c38ac6d
naskapal <co.terbang@gmail.com>
2020-12-08 07:12:03 +0700

    Update bucketManager.js
    readable sucks, using .save instead

--- libraries/bucketManager.js
+++ libraries/bucketManager.js
@@ -5,23 +5,13 @@ const storage = new Storage({
   keyFile: './keyfile.json'
 });
 const bucket = storage.bucket('2020.ngidol.club');
-const {
-  Readable
-} = require('stream');
 
-module.exports.uploadFile = (filename, contentType, buffer) => {
+module.exports.uploadFile = async (filename, contentType, buffer) => {
   const file = bucket.file(filename);
-  const readable = new Readable();
-  readable.push(buffer);
-  readable.push(null);
-  readable
-    .pipe(file.createWriteStream({
-      contentType
-    }))
-    .on('error', function (err) {
-      console.error(err);
-    })
-    .on('finish', function () {
-      console.log(`upload file ${filename} finished`);
-    });
+  return await file.save(buffer, {
+    metadata: {
+      contentType: contentType
+    },
+    resumable: false
+  })
 }
