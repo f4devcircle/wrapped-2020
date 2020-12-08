ff74e40dc26cfb3f24a17a9186ab02d72601c032
Indyka Saidi <indyka@ultravoucher.co.id>
2020-12-08 14:19:06 +0700

    Limit characters

--- libraries/helpers.js
+++ libraries/helpers.js
@@ -5,9 +5,9 @@ const createSlug = (name) => {
 	const randomString = (uuid().split('-'))[4];
 	const slug = `${name.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'')}-${randomString}`;
 	return slug;
-}
+};
 
-const limitText = (originalText, length = 20) => limitCharacters({ text: originalText, length, more: '' });
+const limitText = (originalText, length = 20) => limitCharacters.default({ text: originalText, breakWord: false, length, more: '' });
 
 module.exports = {
 	createSlug,
